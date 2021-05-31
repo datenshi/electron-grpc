fed304232393851ff317166a6428124348fb2b60
Fernando <ferdatenshi@gmail.com>
2021-05-31 11:36:40 +0800

    My cupdates tho the project

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const {app, BrowserWindow} = require('electron')
+const ipc = require('electron').ipcMain;
 const service = require('./proto/chat_grpc_pb')
 const chat = require('./proto/chat_pb')
 const grpc = require('grpc')
@@ -21,6 +22,7 @@ function createWindow() {
     // and load the index.html of the app.
     win.loadFile('index.html')
 
+    // gRPC stuff
     let client = new service.ChatServerClient('localhost:11912', grpc.credentials.createInsecure())
     let note = new chat.Note()
     note.setMessage('Hello from electron')
@@ -28,6 +30,34 @@ function createWindow() {
     client.sendNote(note, function(err, response) {
         console.log('received reply', response);
     });
+
+    let request = new chat.Empty();
+
+    let call = client.chatStream(request, (error, response) => {
+        console.log('Server response to everybody: ' + '[' + response.getName()+ ']: ' + response.getNote());
+        // Send content to window
+        // ipc.send('hello','a string', 10);
+        // win.webContents.on('did-finish-load', ()=>{
+        //     win.webContents.send('ping', 'whoooooooh!');
+        // });
+    });
+
+    call.on('data', response => {
+        console.log(' Response from server - Message: [' + response.getName() + ']: ' + response.getMessage());
+        // Send content to window
+        win.webContents.on('did-finish-load', ()=>{
+            win.webContents.send('ping', '[' + response.getName() + ']: ' + response.getMessage());
+        });
+    });
+
+    call.on('error', error => {
+        console.error(error);
+    });
+
+    call.on('end', () => {
+        console.log('Client: This is the END');
+    });
+
 }
 
 app.on('ready', createWindow)
