686fe8601f32330986ed00326a87f29c68ef5b69
Fernando <ferdatenshi@gmail.com>
2021-06-01 16:33:16 +0800

    - Clean code, add comments and clean input textfield after a message was sent

--- main4Node.js
+++ main4Node.js
@@ -1,8 +1,10 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
 const ipc = require('electron').ipcMain;
-const service = require('./proto/proto/chat2_grpc_pb')
-const chat = require('./proto/proto/chat2_pb')
-const grpc = require('grpc')
+
+// Static Implementation of a gRPC API Under Node.js
+const service = require('./proto/proto/chat2_grpc_pb');
+const chat = require('./proto/proto/chat2_pb');
+const grpc = require('grpc');// Hardcode username for demo purposes
 
 function createWindow() {
     // Create the browser window.
@@ -12,39 +14,38 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: true
         }
-    })
+    });
 
     win.removeMenu();
     // Open the DevTools.
     win.webContents.openDevTools({
         mode: "detach"
-    })
+    });
     // and load the index.html of the app.
-    win.loadFile('index.html')
+    win.loadFile('index.html');
 
-    // gRPC stuff
-    let client = new service.ChatClient('localhost:5001', grpc.credentials.createInsecure())
-    let note = new chat.Message()
-    note.setUser('ElectronUser')
-    note.setText('Hello from electron')
-    console.log('sending note to server')
+    // gRPC client creation
+    let client = new service.ChatClient('localhost:50051', grpc.credentials.createInsecure());
+    
+    //Create the first message to be sent and also allow us to connect to the server
+    let note = new chat.Message();
+    note.setUser('ElectronUser'); // Hardcode username for demo purposes
+    note.setText('Hello from electron');
+    //Start the stream between server and client
+    let call = client.join(note, (error, response) => {
+        console.log('Server response to everybody: ' + '[' + response.getUser()+ ']: ' + response.getText());
+    });
+    // Send the first message to the server
+    console.log('sending note to server');
     client.send(note, function(err, response) {
         console.log('received reply', response);
     });
 
-    let request = new chat.Message();
-    request.setUser('Empty User')
-    request.setText('Empty Text')
-
-    let call = client.join(request, (error, response) => {
-        console.log('Server response to everybody: ' + '[' + response.getUser()+ ']: ' + response.getText());
-    });
-
     ipc.on('send-message', (event, args) => {
         console.log('Send-message from renderer process: ', args);
-        let note = new chat.Message()
-        note.setUser('ElectronUser')
-        note.setText(args)
+        let note = new chat.Message();
+        note.setUser('ElectronUser'); // Hardcode username for demo purposes
+        note.setText(args);
         client.send(note, function(err, response) {
             console.log('received reply', response);
         });
@@ -70,4 +71,4 @@ function createWindow() {
 
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
